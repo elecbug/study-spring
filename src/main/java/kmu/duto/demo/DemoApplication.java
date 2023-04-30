440509561460f9678c57136f39f2e2ac285b2a45
deveb1479@gmail.com <deveb1479@gmail.com>
2023-05-01 00:43:00 +0900

    start

---
+++ src/main/java/kmu/duto/demo/DemoApplication.java
@@ -0,0 +1,13 @@
+package kmu.duto.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
---
+++ src/main/java/kmu/duto/demo/config/MvcConfiguration.java
@@ -0,0 +1,19 @@
+package kmu.duto.demo.config;
+
+import java.util.concurrent.TimeUnit;
+
+import org.springframework.http.CacheControl;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+
+public class MvcConfiguration implements WebMvcConfigurer  {
+
+    @Override
+    public void addResourceHandlers(final ResourceHandlerRegistry registry)
+    {
+        registry.addResourceHandler("/**")
+            .addResourceLocations("classpath:/templates/", "classpath:/static/")
+            .setCacheControl(CacheControl.maxAge(10, TimeUnit.MINUTES));
+    }
+}
